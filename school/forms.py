2d1f45b33e2b17a1240041156226504489636642
sergukr86 <38071335+sergukr86@users.noreply.github.com>
2023-10-22 00:00:07 +0200

    Update forms.py

--- school/forms.py
+++ school/forms.py
@@ -1,67 +1,20 @@
 from django import forms
 
+from .models import Teacher, Subject
 
-class TeacherForm(forms.Form):
-    first_name = forms.CharField(label="Teacher name")
-    last_name = forms.CharField(label="Teacher surname")
-    fathers_name = forms.CharField(label="Teacher fathers name")
-    birth_date = forms.DateField(label="Teacher birth date", initial="yyyy-mm-dd")
 
-    # check input
-    # def clean_first_name(self):
-    #     first_name = self.cleaned_data
-    #     if len(first_name) > 50:
-    #         raise forms.ValidationError("Max length name equals 50 words")
-    #     return None
-    #
-    # def clean_last_name(self):
-    #     last_name = self.cleaned_data
-    #     if len(last_name) > 50:
-    #         raise forms.ValidationError("Name is too long")
-    #
-    # def clean_fathers_name(self):
-    #     fathers_name = self.cleaned_data
-    #     if len(fathers_name) > 50:
-    #         raise forms.ValidationError("Name is too long")
+class TeacherForm(forms.ModelForm):
+    class Meta:
+        model = Teacher
+        fields = ["first_name", "last_name", "fathers_name", "birth_date"]
 
 
 class GroupForm(forms.Form):
     name = forms.CharField(label="Group name", max_length=50)
-    teacher_id_id = forms.IntegerField(label="Teacher's ID")
-
-    # def clean_name(self):
-    #     name = self.cleaned_data
-    #     if len(name) > 50:
-    #         raise forms.ValidationError("Name is too long")
-    #     return None
-    #
-    # def clean_teacher_id_id(self):
-    #     teacher_id_id = self.cleaned_data
-    #     # need another validator to foreign key
-    #     if len(teacher_id_id) > 50:
-    #         raise forms.ValidationError("ID have to be in DB")
+    teacher = forms.ModelChoiceField(queryset=Teacher.objects.all())
 
 
-class SubjectForm(forms.Form):
-    name = forms.CharField(label="Subject name", max_length=50)
-    description = forms.CharField(label="Subject description", widget=forms.Textarea)
-    score = forms.IntegerField(label="Number of score", max_value=10, min_value=1)
-    teacher_id_id = forms.IntegerField(label="Teacher ID")
-
-    # def clean_name(self):
-    #     name = self.cleaned_data
-    #     if len(name) > 50:
-    #         raise forms.ValidationError("Name is too long")
-    #     return None
-    #
-    # def clean_description(self):
-    #     description = self.cleaned_data
-    #     if len(description) > 200:
-    #         raise forms.ValidationError("Name is too long")
-    #     return None
-    #
-    # def clean_score(self):
-    #     score = self.cleaned_data
-    #     if len(score) > 10:
-    #         raise forms.ValidationError("Score is out of range")
-    #     return None
+class SubjectForm(forms.ModelForm):
+    class Meta:
+        model = Subject
+        fields = ["name", "description", "score", "teacher"]
